580a7dccf45da1a1f238e63848f9b0d7f1047160
kunal chugh <157191243+kunal-v2@users.noreply.github.com>
2024-08-18 09:31:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const taskText = document.createElement("span");
     taskText.className = "task-text";
-    taskText.textContent = "100$ bounty is presented to Kunalchugh06@gmail.com";
+    taskText.textContent = "100$ bounty is presented to <ul>100xKunal</ul>";
 
     const deleteButton = document.createElement("button");
     deleteButton.className = "delete-task";
